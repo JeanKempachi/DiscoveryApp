5fcdd3dd75beeef31a56fde3235dcbb383a140be
JEAN BOGUO <215065241@student.uj.ac.za>
2019-07-08 21:53:20 +0200

    Fix layout

--- DiscoveryApplication/DiscoveryApplication/App.xaml.cs
+++ DiscoveryApplication/DiscoveryApplication/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DiscoveryApplication
         {
             InitializeComponent();
 
-            MainPage = new MapPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
--- DiscoveryApplication/DiscoveryApplication/MainPage.xaml.cs
+++ DiscoveryApplication/DiscoveryApplication/MainPage.xaml.cs
@@ -11,15 +11,12 @@ namespace DiscoveryApplication
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
     [DesignTimeVisible(false)]
-    public partial class MainPage : ContentPage
+    public partial class MainPage : TabbedPage
     {
         public MainPage()
         {
             InitializeComponent();
         }
-        private void Goto_Clicked(object sender, EventArgs e)
-        {
-            Navigation.PushAsync(new MapPage());
-        }
+  
     }
 }
